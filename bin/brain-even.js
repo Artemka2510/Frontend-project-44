b8d1dcec3c105b49d0479e0999485dec453e718e
Artem <artem250204@mail.ru>
2023-02-14 09:38:06 +0300

    bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,45 +4,33 @@ import readlineSync from 'readline-sync';
 
 const phrase  ='Answer "yes" if the number is even, otherwise answer "no"';
 const evennumber = (number) => number % 2 === 0;
-const randomnumber = (min, max) => {
+const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max-min)) 
   };
 
-const gameeven = () => {
+const gameEven = () => {
     const userName = hellouser();
     let k = 0;
     console.log(phrase);
     while (k != 3){
-        let number = randomnumber(1, 50);
+        let number = randomNumber(1, 50);
+        let correctAnswer = evenNumber(number) ? 'yes' : 'no';
         console.log(`Question: ${number}`);
         const answer = readlineSync.question('Your answer: ');
         
-        if ((answer === 'no' && evennumber(number) === false) || (answer === 'yes' && evennumber(number) === true)) {
+        if ((answer === 'no' && evenNumber(number) === false) || (answer === 'yes' && evenNumber(number) === true)) {
             console.log('Correct!')
             k += 1;
         if (k === 3) {
             console.log(`Congratulations, ${userName}!`)
         }
-        } else if (answer === 'yes' && evennumber(number) === false) {
-            console.log('"yes" is wrong answer ;(. Correct answer was "no".');
+        } else  {
+            console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
             console.log(`Let's try again, ${userName}!`);
             break;
-        } else if (answer === 'no' && evennumber(number) === true) {
-            console.log('"no" is wrong answer ;(. Correct answer was "yes".');
-            console.log(`Let's try again, ${userName}!`);
-            break;
-        } else if (answer != 'yes' || answer != 'no') {
-            if (evennumber(number) === true) {
-                console.log(`${answer} is wrong answer ;(. Correct answer is "yes"`)
-                console.log(`Let's try again, ${userName}!`)
-            } else {
-                console.log(`${answer} is wrong answer ;(. Correct answer is "no"`)
-                console.log(`Let's try again, ${userName}!`)
-            }
-            break;
-        }
+        } 
     }
 };
-gameeven()
+gameEven()
 
 

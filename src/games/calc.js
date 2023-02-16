b185661711d7c2b2e81bff8db27712af414a2d5d
Artem <artem250204@mail.ru>
2023-02-16 16:56:34 +0300

    src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -27,7 +27,7 @@ const game = () => {
   const b = randomNumber(1, 20);
   const action = randomAction(actions);
   const result = calculator(a, b, action);
-  return [`${a} ${action} ${b}`, result];
+  return [a + action + b, result];
 };
 export default () => {
   getGame(gamePhrase, game);
